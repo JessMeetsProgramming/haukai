477f36fbf6cba52b3e2b5cc0b73018497376f613
JessMeetsProgramming <88293933+JessMeetsProgramming@users.noreply.github.com>
2022-01-26 21:04:45 +1300

    Update slideshow.js
    refresh problem

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,39 +1,40 @@
 // if user resizes the window refresh the page to run script    
 $(window).resize( function() {
 
-    window.location.href = window.location.href;
-});  
+    //window.location.href = window.location.href;
+
+  
        
-// if screen width is greater than 600px run the script
-if (document.documentElement.clientWidth > 600) {
+    // if screen width is greater than 600px run the script
+    if (document.documentElement.clientWidth > 600) {
 
 
-    //run script when document has loaded
-    $(document).ready(function() {
-    
-        // declare slideindex variable to count slides
-        var slideIndex = 0;
-        // run function
-        carousel();
+        //run script when document has loaded
+        $(document).ready(function() {
 
-        // function to chnage the image every 6 seconds
-        function carousel() {
-            var i;
-            var x = document.getElementsByClassName("mySlides");
-            for (i = 0; i < x.length; i++) {
-                x[i].style.display = "none";
+            // declare slideindex variable to count slides
+            var slideIndex = 0;
+            // run function
+            carousel();
+
+            // function to chnage the image every 6 seconds
+            function carousel() {
+                var i;
+                var x = document.getElementsByClassName("mySlides");
+                for (i = 0; i < x.length; i++) {
+                    x[i].style.display = "none";
+                }
+                slideIndex++;
+                if (slideIndex > x.length) {slideIndex = 1}
+                x[slideIndex-1].style.display = "block";
+                setTimeout(carousel, 6000); // Change image every 6 seconds
+
             }
-            slideIndex++;
-            if (slideIndex > x.length) {slideIndex = 1}
-            x[slideIndex-1].style.display = "block";
-            setTimeout(carousel, 6000); // Change image every 6 seconds
-            
-        }
 
-    });
-}
+        });
+    }
 
-
+});
 
 
 
