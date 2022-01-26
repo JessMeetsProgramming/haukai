5e13da61918d18b90cc01e2692ebad4ed0238e6c
JessMeetsProgramming <88293933+JessMeetsProgramming@users.noreply.github.com>
2022-01-26 20:07:30 +1300

    Update map.js
    fixed minify breakage

--- scripts/map.js
+++ scripts/map.js
@@ -1,1 +1,25 @@
-function initMap(){var haukai={lat:-35.218147,lng:173.957414};var map=new google.maps.Map(document.getElementById("map"),{zoom:16,center:haukai,disableDefaultUI:!0,});var marker=new google.maps.Marker({position:haukai,map:map,});var infowindow=new google.maps.InfoWindow({content:"The Haukai"});infowindow.open(map,marker)}
+ // Initialize and add the map
+  function initMap() {
+
+    var haukai = {lat: -35.218147, lng: 173.957414};
+  
+    var map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 16,
+      center: haukai,
+      disableDefaultUI: true,
+    });
+
+    var marker = new google.maps.Marker({
+        position: haukai,
+        map: map, 
+
+    });
+
+    var infowindow = new google.maps.InfoWindow({
+        content:"The Haukai"
+      });
+      
+      infowindow.open(map,marker);
+
+  }
+
