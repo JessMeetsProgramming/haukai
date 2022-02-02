93ac62097f0a558d49784176607f15ce6ccf47b7
JessMeetsProgramming <jess16_greeny@hotmail.com>
2022-02-02 20:21:00 +1300

    Rebuilt footer

--- scripts/nav-menu.js
+++ scripts/nav-menu.js
@@ -15,41 +15,13 @@ document.onreadystatechange = function () {
         }
 
         
-        const navLink = document.querySelectorAll(".nav-link");
+        /*const navLink = document.querySelectorAll(".nav-link");
         //wait for click on menu links
         navLink.forEach(n => n.addEventListener("click", closeMenu));
         // close menu when link is selected
         function closeMenu() {
             hamburger.classList.remove("active");
             navMenu.classList.remove("active");
-        }
+        }*/
     }
 }
-
-/*document.onreadystatechange = function () {
-    if (document.readyState == "complete") {
-        
-        const hamburger = document.querySelector(".hamburger");
-        const close = document.querySelector(".close");
-        const navMenu = document.querySelector(".nav-menu");
-        const centerText = document.querySelector(".w3-right");
-
-        //wait for click on menu icon
-        hamburger.addEventListener("click", mobileMenu);
-    
-        // function to make the mobile menu display 
-        function mobileMenu() {
-            //hide hamburger icon on click event
-            hamburger.className = hamburger.className.replace(" w3-show", " w3-hide");
-            //display close icon on click
-            close.className = close.className.replace(" w3-hide", " w3-show");
-            //display menu 
-            navMenu.className = navMenu.className.replace("w3-hide-small", "");
-            //eliminates right alignment of menu items 
-            centerText.className = centerText.className.replace("w3-right", "w3-center"); 
-        }
-
-      
-
-    }
-}*/
