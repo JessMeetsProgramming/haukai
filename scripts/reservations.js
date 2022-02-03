d3fac02caee92297f64dc055490117db1c517b6d
JessMeetsProgramming <jess16_greeny@hotmail.com>
2022-02-03 20:44:39 +1300

    reservations w3.css

--- scripts/reservations.js
+++ scripts/reservations.js
@@ -11,17 +11,18 @@ function myFunction1() {
       bookings.style.display = "none";
       events.style.display = "block";
       //change button state to reflect the section displayed
-      button1.disabled = true;
+    button1.disabled = true;
       button2.disabled = false;
     } else {
       events.style.display = "none";
     }
     
+ 
   }
   
   function myFunction2() {
     //get div and button elements by id
-    var events = document.getElementById("functions-section");
+   var events = document.getElementById("functions-section");
     var bookings = document.getElementById("booking-section");
     var button2 = document.getElementById("button2");
     var button1 = document.getElementById("button1");
@@ -37,4 +38,9 @@ function myFunction1() {
     } else {
       bookings.style.display = "none";
     }
-  }
+
+
+
+  
+
+}
