94d99c29fd8f411e39b6a04cdbf5bc4352ddda3a
JessMeetsProgramming <jess16_greeny@hotmail.com>
2022-02-06 00:05:13 +1300

    Created staff roster, added pagination js file

---
+++ scripts/roster-pagination.js
@@ -0,0 +1,38 @@
+document.onreadystatechange = function () {
+    // if the page has loaded
+    if (document.readyState == "complete") {
+        // search page for element with class name table1 and declare constant
+        const table1 = document.querySelector(".table1");
+        // display table 1 (current week) on page load
+        table1.classList.toggle("active");
+        // search page for other relevant elements - table 2 and buttons
+        const table2 = document.querySelector(".table2");
+        const next = document.querySelector(".next");
+        const previous = document.querySelector(".previous");
+
+        // wait for click on next or previous buttons
+        next.addEventListener("click", nextFunction);
+        previous.addEventListener("click", previousFunction);
+
+        // function to view roster for next week 
+        function nextFunction() {
+            // make table2 active to display
+            table2.classList.toggle("active");
+            // remove active class from table1 so that it is hidden
+            table1.classList.remove("active");
+            // prevent click when active
+            next.disabled = true;
+            previous.disabled = false;
+        }
+
+        // function to view roster of the current week
+        function previousFunction() {
+            // reverse of the above function
+            table1.classList.toggle("active");
+            table2.classList.remove("active");
+            previous.disabled = true;
+            next.disabled = false;
+        }
+
+    }
+}
