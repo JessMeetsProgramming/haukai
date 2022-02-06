72500f64a7650045af4bae03821ad9a40ec94231
JessMeetsProgramming <jess16_greeny@hotmail.com>
2022-02-06 15:20:11 +1300

    Changed opening hours, fixed slides

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,3 +1,5 @@
+// if user resizes browser window - refresh the page so that the slideshow will stop and start depending on screen size
+window.onresize = function(){ location.reload(); }
 
 // if screen width is greater than 600px run the script
 if (document.documentElement.clientWidth > 600) {
@@ -13,13 +15,23 @@ if (document.documentElement.clientWidth > 600) {
 
         // function to chnage the image every 6 seconds
         function carousel() {
+            // counter
             var i;
+            // each slide
             var x = document.getElementsByClassName("mySlides");
+            // if the number of slides is greater than 0 add 1 to counter (i)
             for (i = 0; i < x.length; i++) {
+                // change slide 1 display value to none
                 x[i].style.display = "none";
+             
             }
+            // increase value of slideIndex by 1
             slideIndex++;
-            if (slideIndex > x.length) {slideIndex = 1}
+            // if slide index is greater than the number of slides
+            if (slideIndex > x.length) {
+                slideIndex = 1
+            }
+            // change slide display to block
             x[slideIndex-1].style.display = "block";
             setTimeout(carousel, 6000); // Change image every 6 seconds
             
